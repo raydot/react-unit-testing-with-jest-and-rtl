c461a76bb678efe3db4e5edacf99db09c0a30623
Dave Kanter <moatgoat@hotmail.com>
2020-01-13 11:41:25 -0800

    Look ma, I wrote a unit test!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "./components/button/Button";
 
 function App() {
   return (
-   
+    <div className="App">
+      <header className="App-header">
+        <Button label="Click Me!" className="button-style"></Button>
+      </header>
+    </div>
   );
 }
 
